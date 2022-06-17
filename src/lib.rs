3ab90e8dbdb943794363c598fe007306dbf68343
Arthur Guedes <arthurguedes375@gmail.com>
2022-06-16 23:05:30 -0300

    Testing data::load_csv

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,9 @@ extern crate nalgebra as na;
 extern crate rand;
 extern crate csv;
 
+#[cfg(test)]
+mod tests;
+
 pub mod sigmoid;
 pub mod neural_network;
 pub mod data;
---
+++ src/tests/data/mod.rs
@@ -0,0 +1,22 @@
+use crate::data::load::load_csv as lcsv;
+#[test]
+fn load_csv() {
+    let data = lcsv("src/tests/data/load_csv_test.csv", Some(0.8));
+
+    assert_eq!(data.train.x.nrows(), 8);
+    assert_eq!(data.test.x.nrows(), 2);
+
+    assert_eq!(data.train.x.ncols(), 4);
+    assert_eq!(data.test.x.ncols(), 4);
+
+    assert_eq!(data.train.y.nrows(), 8);
+    assert_eq!(data.test.y.nrows(), 2);
+    
+    assert_eq!(data.train.y.ncols(), 9);
+    assert_eq!(data.test.y.ncols(), 10);
+    
+    
+    println!("{}", data.train.y);
+    assert_eq!(data.train.y.row(7).column(8)[0], 1.0);
+    assert_eq!(data.test.y.row(0).column(9)[0], 1.0);
+}
