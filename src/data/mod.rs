c3d0c189c026539f09a0534099ae9fc47295d356
Arthur Guedes <arthurguedes375@gmail.com>
2022-06-16 23:42:01 -0300

    Adding debug param to init_theta, and testing it

--- src/data/mod.rs
+++ src/data/mod.rs
@@ -28,7 +28,8 @@ pub fn to_biased(x: DMatrix<Unit>) -> DMatrix<Unit> {
 pub struct Weights {}
 
 impl Weights {
-    pub fn init(x: &X, hidden_layers: u16, hidden_layers_len: u16, y: &Y, epsilon: f32) -> Ws { 
+    pub fn init(x: &X, hidden_layers: u16, hidden_layers_len: u16, y: &Y, epsilon: Option<f32>, debug: bool) -> Ws { 
+        let epsilon = epsilon.unwrap_or(0.1);
         println!(
 "========
 Training examples: {}
@@ -38,7 +39,7 @@ Hidden layers len: {hidden_layers_len}
 ========
 ", x.nrows(), y.ncols()
 );
-        init_task("Initializing theta");
+        if debug { init_task("Initializing theta"); }
         let mut rand = rand::thread_rng();
         let theta_len = hidden_layers + 1;
 
@@ -70,7 +71,7 @@ Hidden layers len: {hidden_layers_len}
             weights.push(w);
         }
 
-        end_task();
+        if debug { end_task(); }
         return weights;
     }
 
--- src/tests/data/mod.rs
+++ src/tests/data/mod.rs
@@ -1,4 +1,5 @@
-use crate::data::load::load_csv as lcsv;
+use crate::data::{load::load_csv as lcsv, Weights};
+
 #[test]
 fn load_csv() {
     let data = lcsv("src/tests/data/load_csv_test.csv", Some(0.8), false);
@@ -17,4 +18,25 @@ fn load_csv() {
     
     assert_eq!(data.train.y.row(7).column(8)[0], 1.0);
     assert_eq!(data.test.y.row(0).column(9)[0], 1.0);
+}
+
+#[test]
+fn init_theta() {
+    let data = lcsv("src/tests/data/load_csv_test.csv", Some(0.8), false);
+    let hidden_layers = 3;
+    let hidden_layers_len = 30;
+    let theta = Weights::init(
+        &data.train.x,
+        hidden_layers,
+        hidden_layers_len,
+        &data.train.y,
+        None,
+        false,
+    );
+
+    assert_eq!(theta[0].nrows(), hidden_layers_len as usize);
+    assert_eq!(theta[0].ncols(), data.train.x.ncols());
+    assert_eq!(theta.len() - 1, hidden_layers as usize);
+    assert_eq!(theta[theta.len() - 1].nrows(), data.train.y.ncols());
+    assert_eq!(theta[theta.len() - 1].ncols(), theta[theta.len() - 2].nrows() + 1);
 }
